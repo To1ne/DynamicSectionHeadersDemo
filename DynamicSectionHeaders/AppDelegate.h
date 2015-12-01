b46373b94091acd27a9840aa0887b7ad85798465
Toon Claes <toon@iotcl.com>
2015-12-01 23:18:02 +0200

    Initial Commit

---
+++ DynamicSectionHeaders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicSectionHeaders
+//
+//  Created by Toon Claes on 01/12/15.
+//  Copyright © 2015 Toon Claes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DynamicSectionHeaders/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicSectionHeaders
+//
+//  Created by Toon Claes on 01/12/15.
+//  Copyright © 2015 Toon Claes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
